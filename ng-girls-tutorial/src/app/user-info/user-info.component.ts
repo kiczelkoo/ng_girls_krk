c47df13e6a127b5abb359e0945e5d51fb9586ef0
Olga <olga.pozn@gmail.com>
2019-10-12 12:02:36 +0200

    added form group

--- ng-girls-tutorial/src/app/user-info/user-info.component.ts
+++ ng-girls-tutorial/src/app/user-info/user-info.component.ts
@@ -1,10 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-info',
   template: `
-  <input type="text" [formControl]="firstName" required>
+  <form [formGroup]="userFormGroup">
+  
+  <label>
+    First Name:
+    <input type="text" formControlName="firstName">
+  </label>
+  <label>
+    Last Name:
+    <input type="text" formControlName="lastName">
+  </label>
+  <label>
+    Email:
+    <input type="text" formControlName="email">
+  </label>
+  <label>
+    Phone:
+    <input type="text" formControlName="phone">
+  </label>
+  <label>
+    Hobby:
+    <input type="text" formControlName="hobby">
+  </label>
+  <label>
+    Best Friend:
+    <input type="text" formControlName="bestFriendName">
+  </label>
+  <label>
+    Date of Birth:
+    <input type="date" formControlName="dateOfBirth">
+  </label>
+
+</form>
   `,
   styleUrls: ['./user-info.component.scss']
 })
@@ -15,6 +46,14 @@ export class UserInfoComponent implements OnInit {
   ngOnInit() {
   }
 
-  firstName = new FormControl('');
+  userFormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    hobby: new FormControl(''),
+    bestFriendName: new FormControl(''),
+    dateOfBirth: new FormControl(''),
+  });
 
 }
